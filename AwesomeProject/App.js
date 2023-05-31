b48b72d99f87ea963d2362d26fd14bd2fba85981
Diana2801 <dianaacna2801@gmail.com>
2023-05-31 17:54:34 -0600

    Creación Proyecto, e instalación Expo

---
+++ AwesomeProject/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style = {styles.letras}>Hola Mundo</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0f0',
+    alignItems: 'center',
+    color: 'blue'
+  },
+  letras:{
+    color: 'blue'
+  }
+});
